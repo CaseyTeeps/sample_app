3292e8ceaaf50f905c738da04853bd3509f4cd34
CaseyTeeps <caseybarth@Caseys-MacBook-Pro.local>
2016-02-15 16:02:19 -0500

    Finished Static Pages & Routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,26 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+      assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+      assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
+    
+  test "should get about" do
+    get :about
+    assert_response :success
+      assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+    
+    test "should get links" do
+        get :links
+        assert_response :success
+        assert_select "title", "Links | Ruby on Rails Tutorial Sample App"
+    end
+    
 
 end
